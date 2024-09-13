9f6c31f3ad13a430053b8fb057f80dc7ccba8157
facundo <facurtprn@gmail.com>
2024-09-13 17:57:20 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyADod875IM0Pwr7m1xGmwhSWOb2zMq8LqY",
+    authDomain: "product-admin-app-7d9c9.firebaseapp.com",
+    projectId: "product-admin-app-7d9c9",
+    storageBucket: "product-admin-app-7d9c9.appspot.com",
+    messagingSenderId: "463931992821",
+    appId: "1:463931992821:web:aedbc6dcd85d12398073bc"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyADod875IM0Pwr7m1xGmwhSWOb2zMq8LqY",
+    authDomain: "product-admin-app-7d9c9.firebaseapp.com",
+    projectId: "product-admin-app-7d9c9",
+    storageBucket: "product-admin-app-7d9c9.appspot.com",
+    messagingSenderId: "463931992821",
+    appId: "1:463931992821:web:aedbc6dcd85d12398073bc"
+  }
 };
 
 /*
